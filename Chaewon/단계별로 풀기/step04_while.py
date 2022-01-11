b922e294c922b89929892d53aaa3340f3454f190
챙 <85454417+codnjs3575@users.noreply.github.com>
2022-01-11 17:03:04 +0900

    Update step04_while.py

--- Chaewon/단계별로 풀기/step04_while.py
+++ Chaewon/단계별로 풀기/step04_while.py
@@ -1,16 +1,12 @@
-# 10430,2588,1330,9498,2753,14681,2884,2739,
-# 10950,8393,15552,2741,2742,11021,2438,2439,
-# 10952,1110
-
 #10952
-# import sys
-# while True :
-#     try :
-#         a = sys.stdin.readline()
-#         c, d = map(int, a.split())
-#         print(c+d)
-#     except :
-#         break
+import sys
+while True :
+    try :
+        a = sys.stdin.readline()
+        c, d = map(int, a.split())
+        print(c+d)
+    except :
+        break
 
 #1110
 N = input() #변경 x, 초기값 비교용
