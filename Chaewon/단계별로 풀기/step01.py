da85c9eb3f356b9752493e2f124e17c18e5707c7
챙 <85454417+codnjs3575@users.noreply.github.com>
2022-01-11 17:04:57 +0900

    Update step01.py

--- Chaewon/단계별로 풀기/step01.py
+++ Chaewon/단계별로 풀기/step01.py
@@ -1,48 +1,48 @@
 #10718
-# for i in range(2):
-#     print("강한친구 대한육군")
+for i in range(2):
+    print("강한친구 대한육군")
 
 #1000
-# a,b= map(int,input().split())
-# print(a+b)
+a,b= map(int,input().split())
+print(a+b)
 
 #1001
-# a,b= map(int,input().split())
-# print(a-b)
+a,b= map(int,input().split())
+print(a-b)
 
 #10998
-# a,b= map(int,input().split())
-# print(a*b)
+a,b= map(int,input().split())
+print(a*b)
 
 #1008
-# a,b= map(int,input().split())
-# print(a/b)
+a,b= map(int,input().split())
+print(a/b)
 
 #10869
-# a,b= map(int,input().split())
-# print(a+b)
-# print(a-b)
-# print(a*b)
-# print('%d' % (a/b) )
-# print(a%b)
+a,b= map(int,input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print('%d' % (a/b) )
+print(a%b)
 
 #10430
-#첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C,
-# 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
-# a,b,c= map(int,input().split())
-# print('%d' % ((a+b)%c))
-# print('%d' % int( ( (a%c) + (b%c) ) % c ) )
-# print('%d' % ((a*b)%c))
-# print('%d' % int( ( (a%c) * (b%c) ) % c ) )
+첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C,
+셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+a,b,c= map(int,input().split())
+print('%d' % ((a+b)%c))
+print('%d' % int( ( (a%c) + (b%c) ) % c ) )
+print('%d' % ((a*b)%c))
+print('%d' % int( ( (a%c) * (b%c) ) % c ) )
 
 #2588
-# num1 = int(input())
-# num2 = int(input())
-#
-# print(num1 * (num2 % 10))
-# print(num1 * ((num2//10) % 10))
-# print(num1 * (num2//100))
-# print(num1 * num2)
+num1 = int(input())
+num2 = int(input())
+
+print(num1 * (num2 % 10))
+print(num1 * ((num2//10) % 10))
+print(num1 * (num2//100))
+print(num1 * num2)
 
 
 
