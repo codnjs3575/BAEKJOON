c93bd5b2aad95dd25afc2cfce7b448b0a6345a90
챙 <85454417+codnjs3575@users.noreply.github.com>
2022-01-11 17:01:34 +0900

    Update step06_function.py

--- Chaewon/단계별로 풀기/step06_function.py
+++ Chaewon/단계별로 풀기/step06_function.py
@@ -1,37 +1,37 @@
 #15596
-# def solve(a):
-#     ans = 0
-#     ans = sum(a)
-#     return ans
-#
-# a = [1,2,3,4,5]
+def solve(a):
+    ans = 0
+    ans = sum(a)
+    return ans
+
+a = [1,2,3,4,5]
 
 #4673
-# num_arr = list(range(1,10000))
-# remove_num_arr = []
-# def self_num(num) :
-#     sum = 0
-#
-#     if int(num) < 10 :
-#         sum = int(num) + int(num)
-#     else :
-#         sum += int(num) #값 하나 더하기
-#         for i in num :
-#             sum += int(i) #각 자리수 더하기
-#     if sum not in remove_num_arr:
-#         remove_num_arr.append(sum)
-#     else : pass
-#     return remove_num_arr
-#
-# for i in num_arr:
-#     remove_num_arr = self_num(str(i))
-#
-# for i in range(len(remove_num_arr)):
-#     if remove_num_arr[i] in num_arr :
-#         num_arr.remove(remove_num_arr[i])
-#
-# for num in num_arr:
-#     print(num)
+num_arr = list(range(1,10000))
+remove_num_arr = []
+def self_num(num) :
+    sum = 0
+
+    if int(num) < 10 :
+        sum = int(num) + int(num)
+    else :
+        sum += int(num) #값 하나 더하기
+        for i in num :
+            sum += int(i) #각 자리수 더하기
+    if sum not in remove_num_arr:
+        remove_num_arr.append(sum)
+    else : pass
+    return remove_num_arr
+
+for i in num_arr:
+    remove_num_arr = self_num(str(i))
+
+for i in range(len(remove_num_arr)):
+    if remove_num_arr[i] in num_arr :
+        num_arr.remove(remove_num_arr[i])
+
+for num in num_arr:
+    print(num)
 
 
 #1065
@@ -52,11 +52,3 @@ for num in range(1,N+1):
 
 print(count)
 
-
-
-
-
-
-
-
-#15596,4673,1065
