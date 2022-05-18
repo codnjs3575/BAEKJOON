9baa981fcd94ed20e2c047da3c663d4b564f85ea
codnjs3575 <codnjs3575@naver.com>
2022-05-18 20:49:16 +0900

    update step01,step02

--- Chaewon/단계별로 풀기/step01.py
+++ Chaewon/단계별로 풀기/step01.py
@@ -1,24 +1,24 @@
-#10718
+# 10718
 for i in range(2):
     print("강한친구 대한육군")
 
-#1000
+# 1000
 a,b= map(int,input().split())
 print(a+b)
 
-#1001
+# 1001
 a,b= map(int,input().split())
 print(a-b)
 
-#10998
+# 10998
 a,b= map(int,input().split())
 print(a*b)
 
-#1008
+# 1008
 a,b= map(int,input().split())
 print(a/b)
 
-#10869
+# 10869
 a,b= map(int,input().split())
 print(a+b)
 print(a-b)
@@ -26,16 +26,16 @@ print(a*b)
 print('%d' % (a/b) )
 print(a%b)
 
-#10430
-첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C,
-셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+# 10430
+# 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C,
+# 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
 a,b,c= map(int,input().split())
 print('%d' % ((a+b)%c))
 print('%d' % int( ( (a%c) + (b%c) ) % c ) )
 print('%d' % ((a*b)%c))
 print('%d' % int( ( (a%c) * (b%c) ) % c ) )
 
-#2588
+# 2588
 num1 = int(input())
 num2 = int(input())
 
@@ -45,7 +45,14 @@ print(num1 * (num2//100))
 print(num1 * num2)
 
 
+# 10926
+import sys
+input = sys.stdin.readline
+T = input().strip()
+print(T+'??!')
 
-
-
-
+# 18108
+import sys
+input = sys.stdin.readline
+n = int(input().strip())
+print(n-543)
--- Chaewon/단계별로 풀기/step02_if.py
+++ Chaewon/단계별로 풀기/step02_if.py
@@ -55,3 +55,34 @@ if M < 0 :
 
 print(H,M)
 
+# 2525
+import sys
+input = sys.stdin.readline
+H,M = map(int,input().split())
+N = int(input().strip())
+
+print((H + (M+N) // 60) % 24,((M+N) % 60))
+
+# 2480
+import sys
+input = sys.stdin.readline
+
+n_arr = input().split()
+n_set = set(n_arr)
+
+# 모두 같은 수 
+if len(n_set) == 1 : money = 10000 + int(n_arr[0])*1000
+
+# 2개만 같은 수 
+elif len(n_set) == 2:
+    if n_arr[0] == n_arr[1] : n = int(n_arr[0])
+    elif n_arr[0] == n_arr[2] : n = int(n_arr[0])
+    else : n = int(n_arr[1])
+    money = 1000 + n * 100
+
+# 모두 다른 수
+else : money = 100 * int(max(n_arr))
+
+print(money)
+
+
